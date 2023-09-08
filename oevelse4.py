6670f61775788196d7471128023bccd73e3b1cf6
HaXr <haxr@outlook.dk>
2023-09-08 14:09:33 +0200

    Øvelse4 done

--- oevelse4.py
+++ oevelse4.py
@@ -4,24 +4,34 @@ from time import sleep
 # Definerer en variabel der holder information omring den røde LED. Såsom at den sidder på pin 26 og er en Pin.OUT type.
 # (Den røde LED er aktiv lav)
 RED_LED = Pin(26, Pin.OUT)
-RED_LED.on()
+RED_LED.off()
 
 # Den gule LED har omvendt spænding og skal derfor starte med at sættes til "off" for at tænde.
 YELLOW_LED = Pin(12, Pin.OUT)
-YELLOW_LED.off()
+YELLOW_LED.on()
 
 GREEN_LED = Pin(13, Pin.OUT)
-GREEN_LED.on()
+GREEN_LED.off()
+
+count = 0
 
 while True:
-  # Sætter værdien af "RED_LED" til den modsatte værdi. (Er LED'en tændt, så vil den slukke og omvendt)
-  RED_LED.value(not RED_LED.value())
-  YELLOW_LED.value(not YELLOW_LED.value())
-  GREEN_LED.value(not GREEN_LED.value())
+  RED_LED.off()
+  YELLOW_LED.on()
+  GREEN_LED.off()
 
-  if RED_LED.value():
-    print("Red LED is ON")
+  if count == 1:
+    RED_LED.value(not RED_LED.value())
+
+  elif count == 2:
+    YELLOW_LED.value(not YELLOW_LED.value())
+  
+  elif count == 3:
+    GREEN_LED.value(not GREEN_LED.value())
+
   else:
-    print("Red LED is OFF")
+    count = 0
+
+  count = count + 1
   
   sleep(1.0)
