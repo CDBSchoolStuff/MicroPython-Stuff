1ccf5e9b89923c18cb10f475b0ce9c6d30b06c37
HaXr <haxr@outlook.dk>
2023-09-22 10:14:30 +0200

    Setup

--- boot.py
+++ boot.py
@@ -26,9 +26,9 @@ def Print_LCD():
     lcd.clear()
     lcd.putstr("Programmering -")
     lcd.move_to(0, 1)
-    lcd.putstr("Lektion X:")    
+    lcd.putstr("Lektion 3:")    
     lcd.move_to(0, 2)
-    lcd.putstr("Emne")
+    lcd.putstr("Neopixel")
     #lcd.move_to(0, 3)
     #lcd.putstr("Something!")
 
--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 # Eksekverer en python fil.
-execfile("oevelse1.py")
+#execfile("oevelse1.py")
+
+from neopixel import NeoPixel
+from machine import Pin
+
+PIXEL_NUMBER = 12 # number of pixels in the Neopixel ring
+PIXEL_PIN = 26 # pin atached to Neopixel ring
+neopixel = NeoPixel(Pin(PIXEL_PIN, Pin.OUT), PIXEL_NUMBER) # create NeoPixel instance
+
+#neopixel[0] = (255, 255, 255) # set pixel 0 to white (r, g b)
+
+#neopixel[3] = (255, 0, 0) # set pixel 3 to red (r, g b)
+
+#neopixel[6] = (0, 255, 0) # set pixel 6 to green (r, g b)
+
+#neopixel[9] = (0, 0, 255) # set pixel 9 to blue (r, g b)
+
+#neopixel[11] = (0, 255, 255) # set pixel 11 to cyan (r, g b)
+
+#neopixel.write() # write to pixels 
+
+def set_color(red, green, blue):
+    for i in range(PIXEL_NUMBER):
+        neopixel[i] = (red, green, blue)
+    neopixel.write()
+
+set_color(200, 0, 0)
+
+def clear():
+    for i in range(PIXEL_NUMBER):
+        neopixel[i] = (0, 0, 0)
+        neopixel.write()
