14ff4a7e8b3a3f95321e94ced98922e01bb1f719
HaXr <haxr@outlook.dk>
2023-09-27 11:22:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-# Eksekverer en python fil.
-# execfile("oevelse1.py")
+from machine import I2C, Pin, SoftI2C
+from time import sleep
+
+i2c = I2C(0)
+
+print("Running I2C scanner\n")
+
+
+def i2c_scanner():
+    # Scan for connected devices
+    devices_identified = i2c.scan()
+
+    #print the result
+    devices_count = len(devices_identified)
+    print("Total number of devides: %d" % devices_count)
+
+    if devices_count == 112:
+        print("Looks like the I2C bus pull-up resistors are missing")
+    else:
+        for i in range(devices_count):
+            print("Device found at address: 0x%02X" % devices_identified[i])
+    print()
+    sleep(1)
+
+while True:
+    i2c_scanner()
