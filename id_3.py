2c0cbc26121f891bb03bcf8c2a6e46f71078caa2
HaXr <haxr@outlook.dk>
2023-10-04 15:15:12 +0200

    Update id_3.py

--- id_3.py
+++ id_3.py
@@ -18,8 +18,10 @@ neopixel = NeoPixel(Pin(PIXEL_PIN, Pin.OUT), PIXEL_NUMBER) # create NeoPixel ins
 
 # Variabel der holder informationer omkring knappen.
 pb1 = Pin(4, Pin.IN)
+pb2 = Pin(0, Pin.IN)
 
 state = False
+pot_val = 0
 
 # Funktion som er ansvarlig for at sætte alle pixels farve på en gang.
 # Funktionen modtager 3 argumenter; red, green, blue. Disse henviser til en RGB farvekode.
@@ -31,42 +33,35 @@ def set_color_all(red, green, blue):
 def clear():
     set_color_all(0,0,0)
 
-def iterate_pixels(rgb, wait):
+def iterate_pixels(rgb, wait, state_arg):
     for number in range(PIXEL_NUMBER): # Itererer mellem alle pixels.
         clear()
         neopixel[number] = (rgb) # Sætter den nuværende pixel i iterationen til den angivede farve.
         neopixel.write() # Skriver værdi til pixel.
         sleep_ms(int(wait/100))
 
-while True: # Uendeligt while loop
-    pot_val = pot.read() # Gemmer aflæsningen af ADC objektets read metode i variablen pot_val
-    spaending = pot_val * (3.3 / 4096) # Udregner spændingen og gemmer i variabel
-    print("Analog potentiometer vaerdi:      ", pot_val) # printer 12Bit ADC værdien
-    # print("\nAnalog potentiometer spaending: ", spaending) # Printer spændingen på GPIO 34
-    color = (0,25,0)
 
-
-    first = pb1.value()
-    sleep(0.01)
-    second = pb1.value()
+while True: # Uendeligt while loop    
+    button1 = pb1.value()
+    button2 = pb2.value()
     
     # Tjekker om knap trykkes
     # hvis knap værdi går fra 1 (True) til 0 (False)
-    if first == 1 and second == 0:
-        print("Knap trykket")
-        if state == False:
-            state = True
-        elif state == True:
-            state = False
-
-    # Tjekker om knap slippes
-    # hvis knap værdi går fra 0 (False) til (True)
-    elif first == 0 and second == 1:
-        print("Knap sluppet")
-
-    if state == True:
-        iterate_pixels(color, pot_val)
+    if button1 == 0:
+        print("Knap1 trykket")
+        state = True
+        print("State=", state)
     
-    print("State=", state)
+    if button2 == 0:
+        print("Knap2 trykket")
+        state = False
+        print("State=", state)
+    
+    if state == True:
+        pot_val = pot.read() # Gemmer aflæsningen af ADC objektets read metode i variablen pot_val
+        print("Analog potentiometer vaerdi:      ", pot_val) # printer 12Bit ADC værdien
+
+    color = (0,25,0)
+    iterate_pixels(color, pot_val, state)
 
 #----------------------------
