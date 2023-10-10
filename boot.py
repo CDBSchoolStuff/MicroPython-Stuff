f9442ea905be8baa8e0264a9443fb0a624e6ea23
HaXr <haxr@outlook.dk>
2023-10-10 13:22:42 +0200

    Øvelse 3

--- boot.py
+++ boot.py
@@ -1,41 +1,41 @@
-from machine import Pin, PWM
-from gpio_lcd import GpioLcd
+# from machine import Pin, PWM
+# from gpio_lcd import GpioLcd
 
-#########################################################################
-# LCD KONTRAST CONFIGURATION
+# #########################################################################
+# # LCD KONTRAST CONFIGURATION
 
-pin_lcd_contrast = 23
-contrast_level = 250                        # Varies from LCD to LCD and wanted contrast level: 0-1023
-lcd_contrast = PWM(Pin(pin_lcd_contrast))   # Create PWM object from a pin
-lcd_contrast.freq(440)                      # Set PWM frequency
-lcd_contrast.duty(contrast_level)
+# pin_lcd_contrast = 23
+# contrast_level = 250                        # Varies from LCD to LCD and wanted contrast level: 0-1023
+# lcd_contrast = PWM(Pin(pin_lcd_contrast))   # Create PWM object from a pin
+# lcd_contrast.freq(440)                      # Set PWM frequency
+# lcd_contrast.duty(contrast_level)
 
-#########################################################################
-# LCD KONTRAST CONFIGURATION SLUT
+# #########################################################################
+# # LCD KONTRAST CONFIGURATION SLUT
 
-def Print_LCD():
-    # Create the LCD object
-    lcd = GpioLcd(rs_pin=Pin(27), enable_pin=Pin(25),
-                  d4_pin=Pin(33), d5_pin=Pin(32), d6_pin=Pin(21), d7_pin=Pin(22),
-                  num_lines=4, num_columns=20)
-    lcd.clear()
-    lcd.putstr("Programmering -")
-    lcd.move_to(0, 1)
-    lcd.putstr("Lektion X:")    
-    lcd.move_to(0, 2)
-    lcd.putstr("Emne")
-    #lcd.move_to(0, 3)
-    #lcd.putstr("Something!")
+# def Print_LCD():
+#     # Create the LCD object
+#     lcd = GpioLcd(rs_pin=Pin(27), enable_pin=Pin(25),
+#                   d4_pin=Pin(33), d5_pin=Pin(32), d6_pin=Pin(21), d7_pin=Pin(22),
+#                   num_lines=4, num_columns=20)
+#     lcd.clear()
+#     lcd.putstr("Programmering -")
+#     lcd.move_to(0, 1)
+#     lcd.putstr("Lektion 6:")    
+#     lcd.move_to(0, 2)
+#     lcd.putstr("I2C & IMU")
+#     #lcd.move_to(0, 3)
+#     #lcd.putstr("Something!")
 
-Print_LCD()
+# Print_LCD()
 
 
 # Ukommenter, hvis der arbejdes med MQTT
 # --------------------------------------
 
-# import sys # Ukommenter, hvis der arbejdes med MQTT
+import sys # Ukommenter, hvis der arbejdes med MQTT
 
-# sys.path.reverse()
-# print("\n\n\nESP32 starter op")
+sys.path.reverse()
+print("\n\n\nESP32 starter op")
 
 # --------------------------------------
--- main.py
+++ main.py
@@ -1,2 +1,55 @@
-# Eksekverer en python fil.
-# execfile("oevelse1.py")
+from machine import I2C
+from machine import Pin
+from time import sleep
+from mpu6050 import MPU6050
+import sys
+from neopixel import NeoPixel
+import umqtt_robust2 as mqtt
+
+#Initialisering af I2C objekt
+i2c = I2C(0)
+
+#Initialisering af mpu6050 objekt
+imu = MPU6050(i2c)
+
+PIXEL_NUMBER = 12 # number of pixels in the Neopixel ring
+PIXEL_PIN = 26 # pin atached to Neopixel ring
+neopixel = NeoPixel(Pin(PIXEL_PIN, Pin.OUT), PIXEL_NUMBER) # create NeoPixel instance
+
+# Diverse variabler:
+standing_threshold = 10000 # Ansvarlig for stejlheden af hvornår enheden betegnes som værende stående eller liggende.
+number_of_falls = 0
+prev_standing = False
+
+def set_color(red, green, blue):
+    for number in range(PIXEL_NUMBER): # Itererer mellem alle pixels.
+        neopixel[number] = (red, green, blue) # Sætter den nuværende pixel i iterationen til den angivede farve.
+    neopixel.write() # Skriver værdi til pixel.
+
+while True:
+    try:
+        # printer hele dictionary som returneres fra get_values metoden
+        imu_data = imu.get_values()
+        print(imu_data)
+
+        if (imu_data.get("acceleration x") > standing_threshold or imu_data.get("acceleration x") < -standing_threshold) or (imu_data.get("acceleration y") > standing_threshold or imu_data.get("acceleration y") < -standing_threshold):
+            print("Enheden står op!")
+            set_color(0,50,0)
+            prev_standing = True
+        else:
+                print("Enheden ligger ned!")
+                set_color(50,0,0)
+                
+                # --------- Del 3 ---------
+                if prev_standing == True: # Sørger for at antallet af fald kun inkrementeres hvis der gåes fra stående til liggende tilstand.
+                    number_of_falls = number_of_falls + 1
+                    print("Antal fald:", number_of_falls)
+                    prev_standing = False
+                    # --------- Del 6 ---------
+                    mqtt.web_print(number_of_falls, 'chbo0003/feeds/ESP32feed')
+                    # -------------------------
+                # -------------------------
+        sleep(1)
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed - exiting program.")
+        sys.exit()
