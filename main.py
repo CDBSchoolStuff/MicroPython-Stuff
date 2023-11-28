806f7ee33c5fe67771aac7b0b6967f76a685a12f
HaXr <haxr@outlook.dk>
2023-11-28 16:04:09 +0100

    PWM og procent styring

--- main.py
+++ main.py
@@ -1,12 +1,54 @@
-from machine import Pin
+from machine import Pin, PWM
+from time import sleep
 
-GREEN_LED = Pin(25, Pin.OUT) # Green
+##################################################################
+# CONFIGURATION
 
-RED_LED = Pin(33, Pin.OUT) # Red
+# Hardware
+LED_RED_PIN = 33
+LED_GREEN_PIN = 25
+LED_BLUE_PIN = 32
 
-BLUE_LED = Pin(32, Pin.OUT) # Blue
+# Red LED
+led_red_freq = 80
+led_red_percentage = 50
 
+# Green LED
+led_green_freq = 60
+led_green_percentage = 0
+
+# Blue LED
+led_blue_freq = 60
+led_blue_percent = 0
+
+##################################################################
+# OBJECTS
+
+LED_RED = PWM(Pin(LED_RED_PIN)) # Red
+
+LED_GREEN = PWM(Pin(LED_GREEN_PIN)) # Green
+
+LED_BLUE = PWM(Pin(LED_BLUE_PIN)) # Blue
+
+
+##################################################################
+# FUNCTIONS
+
+def percentage_to_duty(percent):
+    duty_max = 1023 # Duty værdien kan være mellem 0 og 1023
+    return int((duty_max / 100) * percent)
+
+
+##################################################################
+# PROGRAM
 while True:
-    RED_LED.on()
-    GREEN_LED.on()
-    BLUE_LED.on()
+    LED_RED.freq(led_red_freq)
+    LED_RED.duty(percentage_to_duty(led_red_percentage))
+    
+    LED_GREEN.freq(led_green_freq)
+    LED_GREEN.duty(percentage_to_duty(led_green_percentage))
+    
+    LED_BLUE.freq(led_blue_freq)
+    LED_BLUE.duty(percentage_to_duty(led_blue_percent))
+    
+    sleep(1)
