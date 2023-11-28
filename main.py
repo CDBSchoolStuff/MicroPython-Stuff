f71acd91a39f72e7ead7c18cab1372d0ea2bb8cb
HaXr <haxr@outlook.dk>
2023-11-28 14:58:44 +0100

    Simpel kode til at tænde lysdioder

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# Eksekverer en python fil.
-# execfile("oevelse1.py")
+from machine import Pin
+
+GREEN_LED = Pin(25, Pin.OUT) # Green
+
+RED_LED = Pin(33, Pin.OUT) # Red
+
+BLUE_LED = Pin(32, Pin.OUT) # Blue
+
+while True:
+    RED_LED.on()
+    GREEN_LED.on()
+    BLUE_LED.on()
