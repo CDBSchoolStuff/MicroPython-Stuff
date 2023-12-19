9b48931c251efe94a460a727c513f1f0e0dfcd70
HaXr <haxr@outlook.dk>
2023-12-19 16:26:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C
+from machine import I2C
 from time import sleep, sleep_ms, ticks_ms
 from ina219_lib import INA219
 
@@ -9,6 +9,9 @@ ina = INA219(i2c, 0x40)
 
 start_time = ticks_ms()
 test_time_ms = 1000
+seperation_symbol = ";"
+
+data_list = []
 
 
 ##################################################################
@@ -23,8 +26,21 @@ while True:
     time = ticks_ms() - start_time
     if time < test_time_ms:
         current = ina.get_current()
+        
+        #print(time)
         #print(current)
-        print(time)
+        
+        data_string = str(time) + seperation_symbol + str(current)
+        
+        data_list.append(data_string)
+    
     else:
         print("Test is over,", time, "ms has passed.")
+        #print(current_list)
+
+        with open('data.txt', 'w+') as df:
+            df.write('tid' +  seperation_symbol + 'strøm\n')
+            for line in data_list:
+                df.write(f"{line}\n")
+
         break
