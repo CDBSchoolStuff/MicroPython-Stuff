44cf94a39f0ed03cbbe8b0efeda3ee9eaa8322b3
HaXr <haxr@outlook.dk>
2023-12-19 15:09:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from machine import Pin
-from time import sleep
+from machine import Pin, I2C
+from time import sleep, sleep_ms, ticks_ms
+from ina219_lib import INA219
 
 ##################################################################
 # CONFIGURATION
+i2c = I2C(0, freq = 400000)
+ina = INA219(i2c, 0x40)
 
-# Hardware
-PIN_OUT = 32
-pOut = Pin(PIN_OUT, Pin.OUT)
-
-##################################################################
-# OBJECTS
+start_time = ticks_ms()
+test_time_ms = 1000
 
 
 ##################################################################
@@ -21,10 +20,11 @@ pOut = Pin(PIN_OUT, Pin.OUT)
 # PROGRAM
 
 while True:
-    pOut.value(1)
-    
-    sleep(2)
-    
-    pOut.value(0)
-    
-    sleep(2)
+    time = ticks_ms() - start_time
+    if time < test_time_ms:
+        current = ina.get_current()
+        #print(current)
+        print(time)
+    else:
+        print("Test is over,", time, "ms has passed.")
+        break
